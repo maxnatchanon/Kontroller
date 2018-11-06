9bdfb0978f6861f2388489a32bdf59e7045cd835
Natchanon A <natchanon.347@gmail.com>
2018-11-07 03:47:16 +0700

    Add player animation and background loop

--- src/js/polyShooter.js
+++ src/js/polyShooter.js
@@ -6,15 +6,19 @@ class polyShooter extends Phaser.Scene {
 
 	preload() {
 		this.load.image('player', 'assets/polyShooter/player.png');
+		this.load.image('playerLeft', 'assets/polyShooter/playerLeft.png');
+		this.load.image('playerRight', 'assets/polyShooter/playerRight.png');
 		this.load.image('border', 'assets/polyShooter/border.png');
 		this.load.image('wall', 'assets/polyShooter/wall.png');
 		this.load.image('bg', 'assets/polyShooter/bg.png');
+		this.load.image('star', 'assets/polyShooter/star.png')
 	}
 
 
 	create() {
 
 		this.add.image(480, 270, 'bg');
+		this.star = this.add.tileSprite(480, 270, 960, 1000, 'star');
 
 		this.sideGlow = this.physics.add.staticGroup();
 		this.sideGlow.create(293, 0, 'border').setOrigin(0,0).setScale(-1, 1).refreshBody();
@@ -25,6 +29,7 @@ class polyShooter extends Phaser.Scene {
 		this.wall.create(728, 0, 'wall').setOrigin(0,0).refreshBody();
 
 		this.player = this.physics.add.sprite(480, 480, 'player');
+		this.player.setOrigin(0.5, 0.5);
 
 		this.physics.add.collider(this.player, this.wall);
 
@@ -33,14 +38,21 @@ class polyShooter extends Phaser.Scene {
 	}
 
 	update() {
+		this.star.tilePositionY -= 1;
 		if (this.left.isDown) {
 			this.player.setVelocityX(-180);
+			this.player.setTexture('playerLeft');
+			this.player.setOrigin(0.5, 0.475);
 		}
 		else if (this.right.isDown) {
 			this.player.setVelocityX(180);
+			this.player.setTexture('playerRight');
+			this.player.setOrigin(0.5, 0.475);
 		}
 		else {
 			this.player.setVelocityX(0);
+			this.player.setTexture('player');
+			this.player.setOrigin(0.5, 0.5);
 		}
 	}
 
